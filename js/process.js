17d3dbcf5bcf5ca42f261849cc3c1ee2aac8a10d
Arpo <39718491+arpo9999@users.noreply.github.com>
2020-03-18 21:01:20 +0700

    update js for each tem

--- js/process.js
+++ js/process.js
@@ -1,3 +1,24 @@
 fetch("https://api.thingspeak.com/channels/509782/fields/1/last.txt")
 .then( response => response.text())
-.then( text => document.getElementById('result').value = text)
+.then( text => temperature(text))
+
+function temperature(tem){
+    document.getElementById('result').value = tem;
+    console.log(tem);
+    if(tem < 26)
+    {
+        document.getElementById('state').innerHTML = "อันตราย";
+        document.getElementById('effect').innerHTML = "เย็นชิบหาย";
+    }
+    else if(tem < 30)
+    {
+        document.getElementById('state').innerHTML = "ปลอดภัย";
+        document.getElementById('effect').innerHTML = "สามารถอยู่ได้อย่างปลอดภัย";
+    }
+    else
+    {
+        document.getElementById('state').innerHTML = "อันตราย";
+        document.getElementById('effect').innerHTML = "กลายเป็นปลาย่างไปแล้ว";
+    }
+}
+
