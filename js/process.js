ee6243cdfc7bb8709528b10c13f119b07ae6156d
Arpo <39718491+arpo9999@users.noreply.github.com>
2020-03-21 15:22:31 +0700

    update state and effect, js-css

--- js/process.js
+++ js/process.js
@@ -4,21 +4,29 @@ fetch("https://api.thingspeak.com/channels/509782/fields/1/last.txt")
 
 function temperature(tem){
     document.getElementById('result').value = tem;
+    state = document.querySelector('#state');
+    effect = document.querySelector('#effect');
     console.log(tem);
     if(tem < 26)
     {
         document.getElementById('state').innerHTML = "อันตราย";
         document.getElementById('effect').innerHTML = "เย็นชิบหาย";
+        state.setAttribute("state", "cold");
+        effect.setAttribute("effect", "cold");
     }
     else if(tem < 30)
     {
         document.getElementById('state').innerHTML = "ปลอดภัย";
         document.getElementById('effect').innerHTML = "สามารถอยู่ได้อย่างปลอดภัย";
+        state.setAttribute("state", "safe");
+        effect.setAttribute("effect", "safe");
     }
     else
     {
         document.getElementById('state').innerHTML = "อันตราย";
         document.getElementById('effect').innerHTML = "กลายเป็นปลาย่างไปแล้ว";
+        state.setAttribute("state", "hot");
+        effect.setAttribute("effect", "hot");
     }
 }
 
