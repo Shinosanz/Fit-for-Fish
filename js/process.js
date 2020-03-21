d122da3ec145a4e69e3ce88d071143fe00c60e6f
Arpo <39718491+arpo9999@users.noreply.github.com>
2020-03-21 16:30:26 +0700

    update information page

--- js/process.js
+++ js/process.js
@@ -5,28 +5,24 @@ fetch("https://api.thingspeak.com/channels/509782/fields/1/last.txt")
 function temperature(tem){
     document.getElementById('result').value = tem;
     state = document.querySelector('#state');
-    effect = document.querySelector('#effect');
     console.log(tem);
     if(tem < 26)
     {
         document.getElementById('state').innerHTML = "อันตราย";
-        document.getElementById('effect').innerHTML = "เย็นชิบหาย";
+        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทำให้ปลาอยากอาหารมากขึ้น และอาจส่งผลให้ปลาอยากอาหารมากเกินไปและอาจทำให้ปลาช็อคตายได้";
         state.setAttribute("state", "cold");
-        effect.setAttribute("effect", "cold");
     }
     else if(tem < 30)
     {
         document.getElementById('state').innerHTML = "ปลอดภัย";
         document.getElementById('effect').innerHTML = "สามารถอยู่ได้อย่างปลอดภัย";
         state.setAttribute("state", "safe");
-        effect.setAttribute("effect", "safe");
     }
     else
     {
-        document.getElementById('state').innerHTML = "อันตราย";
-        document.getElementById('effect').innerHTML = "กลายเป็นปลาย่างไปแล้ว";
+        document.getElementById('state').innerHTML = "ร้อนเกินไป";
+        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทำให้ปลามีภูมิต้านทานโรคน้อยลง อาจติดโรคได้ง่ายและถ้าหากอุณหภูมิเปลี่ยนแบบกังทันหันอาจทำให้ปลาช็อคตายได้ ";
         state.setAttribute("state", "hot");
-        effect.setAttribute("effect", "hot");
     }
 }
 
