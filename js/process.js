129a52bc6364349a1b9fc6deece2c5974bdee00d
Arpo <39718491+arpo9999@users.noreply.github.com>
2020-04-12 21:11:19 +0700

    Update process.js

--- js/process.js
+++ js/process.js
@@ -10,21 +10,21 @@ function temperature(tem){
     if(tem < 26)
     {
         document.getElementById('state').innerHTML = "อันตราย";
-        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทำให้ปลามีภูมิต้านทานโรคน้อยลง อาจติดโรคได้ง่ายและถ้าหากอุณหภูมิเปลี่ยนแบบกังทันหันอาจทำให้ปลาช็อคตายได้";
+        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;อุณหภูมิ ณ ขณะนี้อาจทำให้ปลามีภูมิต้านทานโรคน้อยลง อาจติดโรคได้ง่ายและถ้าหากอุณหภูมิเปลี่ยนแบบกังทันหันอาจทำให้ปลาช็อคตายได้";
         state.setAttribute("state", "cold");
         pic_state.setAttribute("src", "pictures/cold.png");
     }
     else if(tem < 30)
     {
         document.getElementById('state').innerHTML = "ปลอดภัย";
-        document.getElementById('effect').innerHTML = "อุณหภูมิ ณ ขณะนี้ปลาสามารถอยู่อาศัยได้อย่างปลอดภัย";
+        document.getElementById('effect').innerHTML = "อุณหภูมิ ณ ขณะนี้ปลาสามารถมีชีวิตรอดได้อย่างปลอดภัย";
         state.setAttribute("state", "safe");
         pic_state.setAttribute("src", "pictures/safe.png");
     }
     else
     {
         document.getElementById('state').innerHTML = "ร้อนเกินไป";
-        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ทำให้ปลาอยากอาหารมากขึ้น อีกทั้งอาจส่งผลให้ปลาอยากอาหารมากเกินไปและอาจทำให้ปลาช็อคตายได้";
+        document.getElementById('effect').innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;อุณหภูมิ ณ ขณะนี้อาจทำให้ปลาอยากอาหารมากขึ้น อีกทั้งยังอาจส่งผลให้ปลาอยากอาหารมากจนเกินไป และอาจทำให้ปลาช็อคตายได้อีกด้วย";
         state.setAttribute("state", "hot");
         pic_state.setAttribute("src", "pictures/hot.png");
     }
